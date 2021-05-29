f2b743198572b5391f1af7c43c6429dd2691908a
lptaylor <lance.taylor98@hotmail.com>
2021-05-29 11:12:03 -0600

    quick change to add defaults for values where it makes sense

--- db/migrate/20210516235021_create_fighters.rb
+++ db/migrate/20210516235021_create_fighters.rb
@@ -3,19 +3,19 @@ class CreateFighters < ActiveRecord::Migration[6.1]
     create_table :fighters do |t|
       t.string :name, limit: 100
       t.string :character, limit: 100
-      t.integer :wins
-      t.integer :losses
-      t.integer :matches
-      t.integer :points_bet_on
-      t.integer :points_won
-      t.integer :points_lost
-      t.integer :kills
-      t.integer :deaths
-      t.integer :suicides
-      t.integer :critical_failures
-      t.integer :streak
-      t.integer :smash_rating
-      t.decimal :total_jail_time
+      t.integer :wins, default: 0
+      t.integer :losses, default: 0
+      t.integer :matches, default: 0
+      t.integer :points_bet_on, default: 0
+      t.integer :points_won, default: 0
+      t.integer :points_lost, default: 0
+      t.integer :kills, default: 0
+      t.integer :deaths, default: 0
+      t.integer :suicides, default: 0
+      t.integer :critical_failures, default: 0
+      t.integer :streak, default: 0
+      t.integer :smash_rating, default: 0
+      t.decimal :total_jail_time, default: 0.0
       t.string :tier, limit: 1 
       t.timestamps
     end
--- db/migrate/20210517004502_create_teams.rb
+++ db/migrate/20210517004502_create_teams.rb
@@ -2,13 +2,13 @@ class CreateTeams < ActiveRecord::Migration[6.1]
   def change
     create_table :teams do |t|
       t.string :team_name, limit: 100
-      t.integer :wins
-      t.integer :losses
-      t.integer :kills
-      t.integer :deaths
-      t.integer :suicides
-      t.integer :pts_won
-      t.integer :pts_lost
+      t.integer :wins, default: 0
+      t.integer :losses, default: 0
+      t.integer :kills, default: 0
+      t.integer :deaths, default: 0
+      t.integer :suicides, default: 0
+      t.integer :pts_won, default: 0
+      t.integer :pts_lost, default: 0
       t.string :tier, limit: 1     
       t.integer :member_fighter_ids, array:true, default:[]
       t.timestamps
